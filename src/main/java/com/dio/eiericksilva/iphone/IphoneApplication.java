dc7a6e3120f96bd4c1a5b477b85d78197836e220
Erick Silva <ericksilvaredes@gmail.com>
2023-09-16 12:33:58 -0300

    feat: exemplo de uso

--- src/main/java/com/dio/eiericksilva/iphone/IphoneApplication.java
+++ src/main/java/com/dio/eiericksilva/iphone/IphoneApplication.java
@@ -3,11 +3,17 @@ package com.dio.eiericksilva.iphone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.dio.eiericksilva.iphone.entities.Iphone;
+
 @SpringBootApplication
 public class IphoneApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IphoneApplication.class, args);
+
+		Iphone myPhone = new Iphone(000000000000000, "Iphone 15 Pro Max", "F45G568HH41H", 100);
+
+		myPhone.tocar();
 	}
 
 }
